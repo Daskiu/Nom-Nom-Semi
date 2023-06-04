1a8f97f25d877fd297797db34d5b42365715f5ec
Daskiu <112220265+Daskiu@users.noreply.github.com>
2023-06-04 17:42:30 -0500

    Delete src/src/mocks directory

--- src/src/mocks/icons.ts
+++
@@ -1,8 +0,0 @@
- export const iconos_propios = 
-        {
-          like: "/Images/like.png",
-          comment: "/Images/Comment.png",
-          foto:"/Images/R1.jpg",
-          perfil:"/Images/profile.png"
-        }
-    
